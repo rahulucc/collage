1019dce90754de2b793017de45f36faa60e97796
rahulucc <53970509+rahulucc@users.noreply.github.com>
2021-12-29 14:57:58 +0530

    Update settings.py

--- register/settings.py
+++ register/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!9+0mz00-+@&n95cht1l6^atykaxepi)_l(8wgf9hbtgns)%*i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['studentregisterinfo.herokuapp.com']
 
 
 # Application definition
